d8c020a040a43e181a707085a2ec886c89fe4850
kwonj1234 <kwonj1234@gmail.com>
2020-05-27 13:56:38 -0400

    landing page styled, add pictures and content

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,20 +6,28 @@ export default function Home() {
     return (
         <div className="Home">
             <div className="hero">
-                <h1 className="hero">Jaehwi Kwon</h1>
-                <p className="text">Elevator pitch</p>
-            </div>
-            <div className="row personalInfo">
-                <Link to="/about" className="aboutJae Link"><h3>About Jae</h3></Link>
-                <div className="column workDone">
-                    <Link to="/projects" className="projects Link"><h3>Projects</h3></Link>
-                    <Link to="cv" className="cv Link"><h3>Employment History</h3></Link>
+                <div className="banner">
+                    <div className="bannerText">
+                        <h1>Jaehwi Kwon</h1>
+                        <br></br>
+                        <p className="text">Elevator pitch</p>
+                    </div>
                 </div>
             </div>
-            <div className="row contact">
-                <Link to="contact_me" className="contactMe Link"><h3>Contact Me</h3></Link>
+            <div className="bottomHero">
+                <div className="linksDisplay">
+                    <div className="row personalInfo">
+                        <Link to="/about" className="aboutJae Link"><h3>About Jae</h3></Link>
+                        <div className="column workDone">
+                            <Link to="/projects" className="projects Link"><h3>Projects</h3></Link>
+                            <Link to="/cv" className="cv Link"><h3>Employment History</h3></Link>
+                        </div>
+                    </div>
+                    <div className="row contact">
+                        <Link to="/contact_me" className="contactMe Link"><h3>Contact Me</h3></Link>
+                    </div>
+                </div>
             </div>
-
         </div>
     )
 }
