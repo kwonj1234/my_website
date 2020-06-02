3621e1d45b663f44f05384c2f851ac637ab6ddf4
kwonj1234 <kwonj1234@gmail.com>
2020-06-02 04:12:02 -0400

    added most of the content, need to style

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -2,8 +2,47 @@ import React from 'react'
 
 export default function Experience() {
     return (
-        <div>
-            
+        <div className="Experience">
+            <h1>Experience</h1>
+            <p>You can download a copy of my resume <a>here</a></p>
+            <div className="row">
+                <div className="leftColumn">
+                    <h4>Nulife Med LLC</h4>
+                    <p>Feburary 2018 - January 2020</p>
+                </div>
+                <div className="rightColumn">
+                    <h5>Sales Service Representative</h5>
+                    <p>
+                        Contrary to the job title, I was not selling the company's products in this
+                        role. My primary role was to visit the homes of post-operative patients, who
+                        were usually operated on by a doctor the company has a relationship with, 
+                        and show them how to use a pneumatic compressor (PC) machine and/or a continuous
+                        passive movement (CPM) machine. I kept in touch with all my assigned patients
+                        during the time they had the machine and kept my supervisor up to date on 
+                        all of my patients conditions or issues.
+                    </p>
+                </div>
+            </div>
+            <div className="row">
+                <div className="leftColumn">
+                    <h4>J.C. Broderick & Associates, Inc.</h4>
+                    <p>June 2018 - September 2018</p>
+                </div>
+                <div className="rightColumn">
+                    <h5>Air Sampling Technician (Inten)</h5>
+                    <p>
+                        As an intern at JCB, I received training and certification to become a
+                        licensed air sampling technician to monitor asbestos levels at job sites
+                        with known asbestos containing materials. In this role, I was very much
+                        left to my own devices. I was let known which project I was assigned to, 
+                        and I followed the schedule of the contractors. If they were on site I had
+                        to be there as well. Scheduling conflicts I passed on to my manager, but
+                        any violations of industrial code rule were usually left up to me to deal with.
+                        I had to learn pretty quickly how to speak critically of men much larger
+                        than me.
+                    </p>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/Passions.js
+++ src/components/Passions.js
@@ -2,8 +2,163 @@ import React from 'react'
 
 export default function Passions() {
     return (
-        <div>
-            
+        <div className="Passions">
+            <h1>Passions</h1>
+            <p>
+                I love a good story, in any medium they come in; video games, movies, books, 
+                or word of mouth. These narratives taught me how to empathize with others and try 
+                to see situations from every point of view. 
+            </p>
+            <h4>Check out some of these stories that have stuck with me.</h4>
+            <p>*Warning: content may include disturbing images, profanity, or references to drugs</p>
+            <div className="row">
+                <div className="column">
+                    <a 
+                        href="https://www.goodreads.com/book/show/18774964-a-man-called-ove" 
+                        className="link aManCalledOve"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        A Man Called Ove
+                        <br/>
+                        By Frederik Backman
+                    </a>
+                    <a 
+                        href="https://www.goodreads.com/book/show/15645.Inferno" 
+                        className="link inferno"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        The Inferno
+                        <br/>
+                        By Dante Alighieri
+                    </a>
+                    <a 
+                        href="http://www.galactanet.com/oneoff/theegg_mod.html" 
+                        className="link theEgg"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        The Egg
+                        <br/>
+                        By Andy Weir
+                    </a>
+                </div>
+                <div className="column">
+                    <a 
+                        href="https://www.reddit.com/r/darksouls3/comments/4nj18t/lore_spoilers_the_complete_story_of_dark_souls/" 
+                        className="link darkSouls"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Dark Souls
+                        <br/>
+                        From Software
+                    </a>
+                    <a 
+                        href="https://thelastofus.fandom.com/wiki/The_Last_of_Us" 
+                        className="link lastOfUs"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        The Last of Us
+                        <br/>
+                        Naughty Dog
+                    </a>
+                    <a 
+                        href="https://www.wired.com/story/pyre-game-review/#:~:text=Pyre%20is%20the%20story%20of,freedom%20on%20their%20own%20terms.&text=Its%20cross%20between%20Ultimate%20Frisbee,the%20highlight%20of%20the%20game."
+                        className="link pyre"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Pyre
+                        <br/>
+                        Supergiant Games
+                    </a>
+                </div>
+                <div className="column">
+                    <a 
+                        href="https://www.imdb.com/title/tt0338013/?ref_=vp_back"
+                        className="link eternalSunshine"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Eternal Sunshine of the Spotless Mind
+                    </a>
+                    <a
+                        href="https://en.wikipedia.org/wiki/John_Wick"
+                        className="link johnWick"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        John Wick
+                    </a>
+                    <a
+                        href="https://en.wikipedia.org/wiki/Comedian_(film)"
+                        className="link comedian"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Jerry Seinfeld: Comedian
+                    </a>
+                </div>
+                <div className="column">
+                    <a
+                        href="https://www.eater.com/2017/2/18/14653382/jeong-kwan-buddhist-nun-chefs-table"
+                        className="link jeongKwan"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Chef's Table: Jeong Kwan
+                    </a>
+                    <a
+                        href="https://variety.com/2020/tv/news/dont-fuck-with-cats-netflix-luka-magnotta-deanna-thompson-1203479502/"
+                        className="link dontFWithCats"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Don't F**k With Cats: Hunting an Internet Killer*
+                    </a>
+                    <a
+                        href="https://www.dancarlin.com/"
+                        className="link danCarlin"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Dan Carlin's Hardcore History
+                    </a>
+                </div>
+                <div className="column">
+                    <a
+                        href="https://www.ted.com/talks/joseph_kim_the_family_i_lost_in_north_korea_and_the_family_i_gained?language=en"
+                        className="link TED"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        TED: Joseph Kim
+                    </a>
+                    <a
+                        href="https://themoth.org/stories/roadside"
+                        className="link theMoth"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        The Moth: Roadside
+                        <br/>
+                        by David Park
+                    </a>
+                    <a
+                        href="https://www.youtube.com/watch?v=VcMIeyjggbM"
+                        className="link thisIsNotHappening"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        This is Not Happening: Meth Pee*
+                        <br/>
+                        by Jessa Reed
+                    </a>
+                </div>
+            </div>
         </div>
     )
 }
