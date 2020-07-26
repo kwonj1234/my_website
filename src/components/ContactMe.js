be1d9f218911e15e626584dad048fd59207765a1
kwonj1234 <kwonj1234@gmail.com>
2020-07-25 21:05:40 -0400

    fixed icons in contactMe

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -2,27 +2,21 @@ import React from 'react'
 import "./ContactMe.css"
 
 export default function ContactMe(props) {
-    return (
-        <div className="ContactMe" ref={props.contactRef}>
-            <br/>
-            <h1>Contact Me!</h1>
-            <div className="contactContent">
-                <p>Whatever your reason for reaching out, I would love to hear from you!</p>
-            </div>
-            <div className="row contactInfo">
-                <a 
-                    href="https://www.linkedin.com/in/jaehwi-kwon/" 
-                    className="icon linkedin-icon" 
-                    target="_blank" 
-                    rel="noopener noreferrer"
-                />
-                <a
-                    href="mailto:jaehwikwn@gmail.com" 
-                    className="icon email-icon" 
-                    target="_blank"
-                    rel="noopener noreferrer"
-                />
-            </div>
-        </div>
-    )
+  return (
+    <div className="ContactMe" ref={props.contactRef}>
+      <br/>
+      <h1>Contact Me!</h1>
+      <div className="contactContent">
+        <p>Whatever your reason for reaching out, I would love to hear from you!</p>
+      </div>
+      <div className="row icon-container">
+        <a href="https://www.linkedin.com/in/jaehwi-kwon/" target="_blank" rel="noopener noreferrer">
+          <img src="https://pluspng.com/img-png/linkedin-icon-png-linkedin-icon-image-512.png" className="icon-linkedin"/>
+        </a>
+        <a href="mailto:jaehwikwn@gmail.com" target="_blank" rel="noopener noreferrer">
+          <img src="https://www.iconsdb.com/icons/download/gray/mail-512.png" className="icon-email"/>
+        </a>
+      </div>
+    </div>
+  )
 }
